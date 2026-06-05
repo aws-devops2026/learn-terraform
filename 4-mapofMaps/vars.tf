ea60f32e67b82e35a9382f75139308df650a3845
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-05 18:54:58 +0530

    terraform

---
+++ 4-mapofMaps/vars.tf
@@ -0,0 +1,21 @@
+# Demo On Map Of Maps
+variable "fruitInfo" {
+  type = map(map(string))
+  default = {
+    Apple = {
+      color = "Red"
+      taste = "Sweet"
+      price = "100"
+    }
+    Banana = {
+      color = "Yellow"
+      taste = "Sweet"
+      price = "80"
+    }
+    Lemon = {
+      color = "Yellow"
+      taste = "Sour"
+      price = "50"
+    }
+  }
+}
