a9e3938c4a0702bbaba4a1a880fc5149b4a8aa1c
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-05 19:13:23 +0530

    terraform

--- 4-mapofMaps/vars.tf
+++ 4-mapofMaps/vars.tf
@@ -5,17 +5,17 @@ variable "fruitInfo" {
     Apple = {
       color = "Red"
       taste = "Sweet"
-      price = "100"
+      price = "120"
     }
     Banana = {
       color = "Yellow"
       taste = "Sweet"
-      price = "80"
+      price = "90"
     }
     Lemon = {
       color = "Yellow"
       taste = "Sour"
-      price = "50"
+      price = "30"
     }
   }
 }
