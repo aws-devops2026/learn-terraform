64248b068c16d03648b5ab216396e15abc6f8a36
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 12:37:33 +0530

    terraform

--- 6-functions/sample.tf
+++ 6-functions/sample.tf
@@ -11,10 +11,6 @@ output "appleProperties" {
 value = "Apple is ${var.fruits["apple"]["color"]} in color, tastes ${var.fruits["apple"]["taste"]}, costs ${var.fruits["apple"]["price"]} per ${var.fruits["apple"]["metrics"]} and is available in ${var.fruits["apple"]["state"]}"
 }
 
-  
- output "grapesProperties" {
-    value = "grapes are ${var.fruits["grapes"]["color"]} in color, taste ${var.fruits["grapes"]["taste"]}, cost ${var.fruits["grapes"]["price"]} per ${var.fruits["grapes"]["metrics"]} and are available in ${var.fruits["grapes"]["state"]}"
-}
 
 output "blueberryProperties" {
     value = "Blueberries are ${lookup(var.fruits["blueberry"], "color", "unknown")} in color, taste ${lookup(var.fruits["blueberry"], "taste", "unknown")}, cost ${lookup(var.fruits["blueberry"], "price", "unknown")} per ${lookup(var.fruits["blueberry"], "metrics", "pound")} and are available in ${lookup(var.fruits["blueberry"], "state", "PUNJAB")}"
