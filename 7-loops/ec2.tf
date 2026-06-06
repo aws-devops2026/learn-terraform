0d4f7dea7b35e5c10431171622be97fbad8f49d6
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 13:01:57 +0530

    terraform

--- 7-loops/ec2.tf
+++ 7-loops/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "main" {
   ami           = "ami-0fcc78c828f981df2"
   count = 3
-  
-  instance_type = "t3.medium"
+
+  instance_type = "t3.micro"
 
   tags = {
     Name = "B60-Demo-Ec3"
