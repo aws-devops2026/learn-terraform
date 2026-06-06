b7443ffd0c3e851cf8ff1e4adac404ff54e4f1b4
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 12:24:00 +0530

    terraform

--- 6-functions/sample.tf
+++ 6-functions/sample.tf
@@ -3,4 +3,12 @@ variable "fruits" {}
 
 output "fruits" {
   value = var.fruits
+
 }
+
+
+output "appleProperties" {
+value = "Apple is ${var.fruits["apple"]["color"]} in color, tastes ${var.fruits["apple"]["taste"]}, costs ${var.fruits["apple"]["price"]} per ${var.fruits["apple"]["metrics"]} and is available in ${var.fruits["apple"]["state"]}"
+}
+
+  
