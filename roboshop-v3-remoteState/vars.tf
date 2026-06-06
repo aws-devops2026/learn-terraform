ea85a5d93da21c410b4b2e6b4f88826b2b693370
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 23:17:04 +0530

    terraform

--- roboshop-v3-remoteState/vars.tf
+++ roboshop-v3-remoteState/vars.tf
@@ -6,7 +6,7 @@ variable "components" {
             ami_id = "ami-0fcc78c828f981df2"
         }
         shipping = {
-            instance_type = "t3.large"
+            instance_type = "t3.small"
             ami_id = "ami-0fcc78c828f981df2"
         }
         frontend = {
@@ -14,11 +14,11 @@ variable "components" {
             ami_id = "ami-0fcc78c828f981df2"
         }
         cart = {
-            instance_type = "t3.medium"
+            instance_type = "t3.micro"
             ami_id = "ami-0fcc78c828f981df2"
         }
         payment = {
-            # instance_type = "t3.medium"
+            # instance_type = "t3.small"
             ami_id = "ami-0fcc78c828f981df2"
         }
     }
