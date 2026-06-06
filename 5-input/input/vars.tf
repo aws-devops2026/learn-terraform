6f947d8aedde8d9fddf727169841a84ba8586430
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 11:47:50 +0530

    terraform

--- 5-input/input/vars.tf
+++ 5-input/input/vars.tf
@@ -1,14 +1,3 @@
-variable "cityName" {
+#variable "cityName" {
     type        = string
-    description = "The name of the city"
-    default     = "Bangalore"
-}
-
-output "cityName" {
-  value = var.cityName
-}
-
-variable "environment" {
-    type        = string
-    description = "The environment for deployment"
-}
+    description = "
