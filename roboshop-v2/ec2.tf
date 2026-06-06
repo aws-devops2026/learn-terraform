a63d458ebaefbe0c1dadb3185926f91080cda7d2
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 20:17:29 +0530

    terraform

--- roboshop-v2/ec2.tf
+++ roboshop-v2/ec2.tf
@@ -2,7 +2,12 @@ resource "aws_instance" "main" {
   for_each = var.components
 
   ami           = each.value["ami_id"]
-  instance_type = each.value["instance_type"]
+  instance_type = lookup(each.value, "instance_type", "t3.small")
+
+ # instance_type = lookup(each.value, "instance_type", "t3.micro")
+
+# Exception Handling : If instance_type is not mentioned for any component, 
+then it will take t3.micro as default value.
 
   tags = {
     Name = each.key
