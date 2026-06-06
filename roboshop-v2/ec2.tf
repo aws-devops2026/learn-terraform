feffcbe80a4eaefa263790ad6e0c196db94769c2
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 17:39:30 +0530

    terraform

---
+++ roboshop-v2/ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "main" {
+    count = length(var.components)
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.components[count.index]
+  }
+}
+
+#resource "aws_instance" "main" {
+  #count        = length(var.components)
+ # ami           = var.ami_id
+  #instance_type = var.instance_type
+
+ # tags = {
+   # Name = var.components[count.index]
+ # }
+#}
+
+
+
