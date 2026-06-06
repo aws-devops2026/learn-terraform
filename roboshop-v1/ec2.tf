675812d4450dec5c3bf65e63f1c3246a0f811ee9
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 14:33:32 +0530

    terraform

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "main" {
     count = length(var.components)
   ami           = var.ami_id
-  instance_type = "var.instance_type"
+  instance_type = var.instance_type
 
   tags = {
     Name = var.components[count.index]
