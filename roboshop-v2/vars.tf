1e043d8a0313d860605ce286757c244ba0f3711a
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-06 19:20:47 +0530

    terraform

--- roboshop-v2/vars.tf
+++ roboshop-v2/vars.tf
@@ -1,12 +1,32 @@
+# Map Input : Map Of Maps 
+
 variable "components" {
-    default = ["frontend", "catalogue", "shipping", "user", "cart", "payment"]
+    default = {
+        frontend = {
+            instance_type = "t3.micro"
+            ami_id = "ami-0fcc78c828f981df2"
+        }
+    
+    catalogaue = {
+    instance_type = "t3.medium"
+            ami_id = "ami-0fcc78c828f981df2"
+        }
+
+    shipping = {
+    instance_type = "t3.micro"
+            ami_id = "ami-0fcc78c828f981df2"
+        }
+
+    payment = {
+    instance_type = "t3.micro"
+            ami_id = "ami-0fcc78c828f981df2"
+        }
+        }
 }
 
-variable "ami_id" {
-  default = "ami-0fcc78c828f981df2"
-}
 
-variable "instance_type" {
-    default = "t3.micro"
-  
-}
+
+#variable "ami_id" {
+  #default = "ami-0fcc78c828f981df2"
+#} 
+
