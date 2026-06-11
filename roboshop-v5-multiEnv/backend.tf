e80325ba309b79d7e81639db4c4a9aef9cd41fbd
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-11 16:46:19 +0530

    terraform

--- roboshop-v5-multiEnv/backend.tf
+++ roboshop-v5-multiEnv/backend.tf
@@ -2,3 +2,7 @@ terraform {
   backend "s3" { }
 
 }
+
+# Backend attribute is moved to env specific tfvars file, so that we can have different backend for
+# different environment. This is required when we want to maintain separate state file for each 
+# environment in same S3 bucket.
