208b3dce162206a890e697a0a33f4e95d7264898
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-11 12:24:11 +0530

    terraform

--- roboshop-v5-multiEnv/provider.tf
+++ roboshop-v5-multiEnv/provider.tf
@@ -1,1 +1,2 @@
-# DNS Record by default should be with private ip address only. But, for tools & frontend, I want the DNS Record to be created with public ip address
+# DNS Record by default should be with private ip address only.
+# But, for tools & frontend, I want the DNS Record to be created with public ip address
