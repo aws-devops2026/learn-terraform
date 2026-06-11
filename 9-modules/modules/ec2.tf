beaca72b077480dcffaa4c61320491f251efd113
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-11 11:01:47 +0530

    terraform

---
+++ 9-modules/modules/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "main" {
+
+  ami           = data.aws_ami.latest.id
+  instance_type = var.instance_type
+  # Exception Handling : If instance_type is not mentioned for any component, then it will take t3.micro as default value.
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ 9-modules/modules/vars.tf
@@ -0,0 +1,4 @@
+# Empty Variables
+variable "instance_type" {}
+variable "instance_name" {}
+variable "ami_name" {}
