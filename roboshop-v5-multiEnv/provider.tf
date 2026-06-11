93410e856fe4a7df83cfd6e419a8c6d0587c0ea3
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-11 12:25:00 +0530

    terraform

--- roboshop-v5-multiEnv/provider.tf
+++ roboshop-v5-multiEnv/provider.tf
@@ -1,2 +1,2 @@
-# DNS Record by default should be with private ip address only.
-# But, for tools & frontend, I want the DNS Record to be created with public ip address
+# Configure the AWS Provider
+provider "aws" {}
