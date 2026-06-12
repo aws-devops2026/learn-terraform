dc8c33fbf667aa9c8f2d7ea238b86761ce9f352c
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-12 07:28:52 +0530

    terraform

--- roboshop-v5-multiEnv/modules/data.tf
+++ roboshop-v5-multiEnv/modules/data.tf
@@ -7,7 +7,7 @@ data "aws_ami" "latest" {
         values = ["${var.ami_name}"] # Name of the AMI, you can find it in the AWS console or by using AWS CLI
     }
         
-    owners = ["self"] # Owner ID of the AMI, you can find it in the AWS console or by using AWS CLI
+    
 }
 
 # Datasource for security group, we will use this in the EC2 module to attach the security group to the instance
