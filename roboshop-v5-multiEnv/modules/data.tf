c0908069b9a8d8f9423c3f64ad11a9cef817331e
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-12 13:28:11 +0530

    terraform

--- roboshop-v5-multiEnv/modules/data.tf
+++ roboshop-v5-multiEnv/modules/data.tf
@@ -4,17 +4,19 @@ data "aws_ami" "latest" {
     
     filter {
         name   = "name"
-        values = ["${var.ami_name}"] # Name of the AMI, you can find it in the AWS console or by using AWS CLI
+        values = ["${var.ami_name}"] 
+        
+        # Name of the AMI, you can find it in the AWS console or by using AWS CLI
     }        
 
 }
 
-# Datasource for security group, we will use this in the EC2 module to attach the security group to the instance
+# Datasource for security group
 data "aws_security_group" "selected" {
   name = var.sg_name
 }
 
-# Datasource for route53 zone, we will use this in the route53 module to create the record in the selected zone 
+# Datasource for route53 zone, 
 data "aws_route53_zone" "selected" {
   name         = var.domain_name
   private_zone = false
