3b39c12a853ad858f6120d856a6d52804e004768
vijayt <vijaykumar.thunugur@gmail.com>
2026-06-12 14:01:20 +0530

    terraform

--- roboshop-v5-multiEnv/modules/data.tf
+++ roboshop-v5-multiEnv/modules/data.tf
@@ -5,7 +5,7 @@ data "aws_ami" "latest" {
 
     filter {
         name   = "name"
-        values = [var.ami_name] 
+        values =["${var.ami_name}"] 
         
         # Name of the AMI, you can find it in the AWS console or by using AWS CLI
     }        
